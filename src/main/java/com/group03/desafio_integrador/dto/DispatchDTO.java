73edce5c05b31a35982adb96253c036073db25d2
IngridPaulino <ingrid.paulino@mercadolivre.com>
2022-11-16 15:23:05 -0300

    :triangular_flag_on_post: remove: remove attribute of DispatchDTO

--- src/main/java/com/group03/desafio_integrador/dto/DispatchDTO.java
+++ src/main/java/com/group03/desafio_integrador/dto/DispatchDTO.java
@@ -1,17 +1,15 @@
 package com.group03.desafio_integrador.dto;
 
-import com.group03.desafio_integrador.entities.entities_enum.CategoryEnum;
-import com.group03.desafio_integrador.entities.entities_enum.DispatchStatusEnum;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import java.math.BigInteger;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DispatchDTO {
-    private Long id_Packing;
-    private Long buyer_id;
-    private CategoryEnum category;
-    private DispatchStatusEnum status;
+    private BigInteger buyer_id;
+    private Integer category;
 }
